a5c69ce9e4ee47fdd62e17a9f0ffb82df4323574
Ulum <uniquefactuhl@gmail.com>
2026-07-25 01:26:03 +0700

    fix: import Request in PopulationRecordController to resolve 500 error

--- app/Http/Controllers/Admin/PopulationRecordController.php
+++ app/Http/Controllers/Admin/PopulationRecordController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class PopulationRecordController extends Controller
